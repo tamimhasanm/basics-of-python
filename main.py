3dd7c14f962b848e9abd784e0b283638629f4a23
Tamim Hasan <tamim.shanto@gmail.com>
2025-08-23 20:26:04 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+#Let's code with str variable
+'''
+a = "Hello, World!"
+print(a)
+'''
+'''
+Variables in python 
+a = "Shanto" (str)
+b = 15 (int)
+c = 15.66 (float)
+d = 10>5 (conditional statement/ boolean{True/False})
+
+Operators in Python
+1. Arithmetic Operator:
++ = Addition =      a+b
+- = Subtraction =   a-b
+* = Multiplication= a*b
+/ = Division =      a/b
+% = Modulus =       a%b
+**= Exponents =     a**b
+//= Floor Division= a//b
+
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+output is
+30
+-10
+200
+0.5
+10
+100000000000000000000
+0
+'''
+
